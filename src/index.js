6e5022f25a03492d4b1f117134075223a8e85eba
onifadjosh <onifadjosh@gmail.com>
2023-02-14 22:58:57 +0100

    navMenu changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+const navMenu = document.querySelector('.navMenu');
+const Hamburger = document.querySelector('.hamburger');
 
+Hamburger.addEventListener('click', ()=>{
+    Hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
 
